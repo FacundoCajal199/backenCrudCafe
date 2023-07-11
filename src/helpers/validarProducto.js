89529ac06a7e21416060735f1108cda6b9e4ade5
Facund05 <118226317+FacundoCajal199@users.noreply.github.com>
2023-07-11 01:31:06 -0300

    dev lista

--- src/helpers/validarProducto.js
+++ src/helpers/validarProducto.js
@@ -1,38 +1,40 @@
 import { check } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion";
 const validarProducto = [
-    check("nombreProducto")
-      .notEmpty()
-      .withMessage("el nombre del producto es obligatorio")
-      .isLength({ min: 2, max: 100 })
-      .withMessage("el nombre del producto debe contener entre 2 y 100"),
+  check("nombreProducto")
+    .notEmpty()
+    .withMessage("el nombre del producto es obligatorio")
+    .isLength({ min: 2, max: 100 })
+    .withMessage("el nombre del producto debe contener entre 2 y 100"),
 
-    check("precio")
-      .notEmpty()
-      .withMessage("el precio es un dato obligatorio")
-      .isNumeric()
-      .withMessage("el precio debe tener un formato numerico")
-      .custom((value) => {
-        if (value >= 1 && value <= 10000) {
-          return true;
-        } else {
-          throw new Error("el precio debe ser entre 1 y 10000");
-        }
-      }),
-    check("imagen")
-      .notEmpty()
-      .withMessage("la direccion de la imagen es obligatoria")
-      .matches(
-        /^(https?:\/\/)?(?:www\.)?[\w-]+\.[\w.-]+(?:\/[\w-./?%&=]*)?\.(?:jpg|jpeg|png|gif|bmp|jpeg\?[\w=&.]*)$/
-      )
-      .withMessage("debe ingresar una url de imagen valida"),
-  check('categoria')
-     .notEmpty()
-     .withMessage('La categoria es obligatoria')
-     .isIn(["bebida caliente", "bebida fria", "dulce", "salado"])
-     .withMessage("La categoria debe ser una de las siguientes opciones [bebida caliente, bebida fria, dulce, salado]"),
-     (req, res,next)=>{
-        resultadoValidacion(req, res,next)
-     }
-  ];
+  check("precio")
+    .notEmpty()
+    .withMessage("el precio es un dato obligatorio")
+    .isNumeric()
+    .withMessage("el precio debe tener un formato numerico")
+    .custom((value) => {
+      if (value >= 1 && value <= 10000) {
+        return true;
+      } else {
+        throw new Error("el precio debe ser entre 1 y 10000");
+      }
+    }),
+  check("imagen")
+    .notEmpty()
+    .withMessage("la direccion de la imagen es obligatoria")
+    .matches(
+      /^(https?:\/\/)?(?:www\.)?[\w-]+\.[\w.-]+(?:\/[\w-./?%&=]*)?\.(?:jpg|jpeg|png|gif|bmp|jpeg\?[\w=&.]*)$/
+    )
+    .withMessage("debe ingresar una url de imagen valida"),
+  check("categoria")
+    .notEmpty()
+    .withMessage("La categoria es obligatoria")
+    .isIn(["bebida caliente", "bebida fria", "dulce", "salado"])
+    .withMessage(
+      "La categoria debe ser una de las siguientes opciones [bebida caliente, bebida fria, dulce, salado]"
+    ),
+  (req, res, next) => {
+    resultadoValidacion(req, res, next);
+  },
+];
 export default validarProducto;
--- src/helpers/validarUsuario.js
+++ src/helpers/validarUsuario.js
@@ -2,26 +2,32 @@ import { check } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion";
 
 const validarUsuario = [
-    check("nombreUsuario")
+  check("nombreUsuario")
     .notEmpty()
     .withMessage("El nombre de usuario es requerido")
     .isString()
     .withMessage("El nombre de usuario debe ser una cadena de texto")
-    .isLength({max:16,min:2})
+    .isLength({ max: 16, min: 2 })
     .withMessage("El nombre de usuario debe contener entre 2 y 16 caracteres"),
 
-    check("password")
+  check("password")
     .notEmpty()
     .withMessage("La contraseña es requerida")
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]{8,16}$/)
-    .withMessage("La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un carácter especial y un número. Además, la longitud máxima es de 16 caracteres."),
-    
-    check("email")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]{8,16}$/
+    )
+    .withMessage(
+      "La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un carácter especial y un número. Además, la longitud máxima es de 16 caracteres."
+    ),
+
+  check("email")
     .notEmpty()
     .withMessage("El campo email es requerido")
     .isEmail()
     .withMessage("El email no es valido"),
-    (req,res,next)=>resultadoValidacion(req,res,next)
-]
+  (req, res, next) => {
+    resultadoValidacion(req, res, next);
+  },
+];
 
-export default validarUsuario
+export default validarUsuario;
